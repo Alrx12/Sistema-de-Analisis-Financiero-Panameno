4a0ef793f09f850664552c4b7fdfe954769d32dd
Alexis Pineda <lex12pineda@hotmail.com>
2026-04-19 11:51:49 -0500

    fix: reemplazar file: deps con babel-plugin-module-resolver

--- mobile/babel.config.js
+++ mobile/babel.config.js
@@ -6,6 +6,21 @@ module.exports = function (api) {
       ["babel-preset-expo", { jsxImportSource: "nativewind" }],
     ],
     plugins: [
+      // Resuelve @safpro/* a los módulos locales en ./packages/
+      // Esto elimina la necesidad de dependencias file: en package.json
+      [
+        "module-resolver",
+        {
+          root: ["./"],
+          extensions: [".ts", ".tsx", ".js", ".jsx"],
+          alias: {
+            "@safpro/api": "./packages/api",
+            "@safpro/types": "./packages/types",
+            "@safpro/stores": "./packages/stores",
+            "@safpro/categories": "./packages/categories",
+          },
+        },
+      ],
       // Reanimated siempre debe ir último
       "react-native-reanimated/plugin",
     ],
